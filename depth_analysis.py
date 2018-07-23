f40cde18582ecd0de735e143f83247a0e02513c3
Louise Bowler <lbowler@turing.ac.uk>
2018-07-23 15:59:01 +0100

    Reorganisation of depth_analysis - each table is generated by a separate function, some duplicated functionality removed.

--- depth_analysis.py
+++ depth_analysis.py
@@ -21,42 +21,7 @@ def listdir_nohidden(path):
     return newfolds
 
 
-def examine_hyperparameter_optimisation(results, best_id):
-
-    # Extract the loss values from the full list of results, and calculate the running minimum value
-    loss = numpy.asarray([r["loss"] for r in results])
-    running_min_loss = numpy.minimum.accumulate(loss)
-    lowest_loss = loss[best_id]
-    all_best_ids = numpy.where(loss == lowest_loss)[0]
-
-    # Plot the loss and running los values against the iteration number, and save to the output folder
-    plt.plot(range(0, len(loss)), loss, label="loss")
-    plt.plot(range(0, len(running_min_loss)), running_min_loss, label="running min(loss)")
-    plt.plot(best_id, lowest_loss, "ro", label="min(loss)")
-    if len(all_best_ids) > 1:
-        plt.plot(all_best_ids, lowest_loss*numpy.ones(all_best_ids.shape), "rx", label="repeated min(loss)")
-    plt.legend()
-    plt.title("Hyperparameter optimisation")
-    plt.xlabel("Iteration")
-    plt.ylabel("Loss")
-    plt.savefig(os.path.join("output", "hyperparameter_loss_values.pdf"))
-
-    # Give details of other hyperparameter combinations that also achieved this loss
-    if len(all_best_ids) > 1:
-        print "\nWARNING: multiple hyperparameter combinations achieved the same lowest loss value as trial", best_id
-        all_best_ids = numpy.where(loss == lowest_loss)[0]
-        print "ID               ",
-        for id in all_best_ids:
-            print "%-17d" % id,
-        print ""
-        for param in results[all_best_ids[0]]["Params"]:
-            print "%-17s" % param,
-            for id in all_best_ids:
-                print "%-17.4f" % results[id]["Params"][param],
-            print ""
-            
-
-if __name__ == "__main__":
+def load_data():
 
     # load labels and split for task A
     path_to_split = os.path.join('downloaded_data', 'semeval2017-task8-dataset', 'traindev')
@@ -210,15 +175,11 @@ if __name__ == "__main__":
     dev_tweets = dev.keys()
     train_tweets = train.keys()
 
-    testfile = 'subtaska.json'
-    with open(testfile) as f:
-        for line in f:
-            test_truevals = json.loads(line)
-    test_tweets = test_truevals.keys()
-
-    # Read prediction json dict
+    # Read the predictions of the model
     submission_file = os.path.join("output", "predictions.txt")
     submission = json.load(open(submission_file, 'r'))
+
+    # And then the corresponding test data
     testfile = 'subtaska.json'
     with open(testfile) as f:
         for line in f:
@@ -239,17 +200,68 @@ if __name__ == "__main__":
             if tweetid in branch:
                 depthinfo[tweetid] = branch.index(tweetid)
 
+    return depthinfo, submission, test_truevals, dev
 
-    # group true labels and predictions according to their depth
-    #%%
+
+def load_trials_data():
+
+    trials_file = os.path.join("output", "trials.txt")
+
+    if os.path.exists(trials_file):
+        # Load the trials data
+        with open(trials_file, 'rb') as f:
+            trials = pickle.load(f)
+
+        # We need to examine the best trial
+        best_trial_id = trials.best_trial["tid"]
+        best_trial_loss = trials.best_trial["result"]["loss"]
+        dev_result_id = pickle.loads(trials.attachments["ATTACH::%d::ID" % best_trial_id])
+        dev_result_predictions = pickle.loads(trials.attachments["ATTACH::%d::Predictions" % best_trial_id])
+
+        # Change ID format from ints to strings
+        strpred = [convertlabeltostr(s) for s in dev_result_predictions]
+
+        # Transform to submission format and save
+        dev_results_dict = dict(zip(dev_result_id, strpred))
+        with open("result_dev.json", "w") as outfile:
+            json.dump(dev_results_dict, outfile)
+    else:
+        best_trial_id = None
+        best_trial_loss = None
+        dev_results_dict = None
+        print "trials.txt is not available"
+
+    return best_trial_id, best_trial_loss, dev_results_dict
+
+
+def get_true_and_predicted_classes(true_tweet_classes, predicted_tweet_classes):
+
+    # Sometimes this function may be called with empty predicted classes (e.g. if trials is not available).
+    # In that case, return None for the true and predicted lists.
+    if predicted_tweet_classes is None:
+        true = None
+        pred = None
+        return true, pred
+
+    true = []
+    pred = []
+
+    # Generate lists of true and predicted classes for all tweets in this set
+    for k in true_tweet_classes.keys():
+        true.append(true_tweet_classes[k])
+        pred.append(predicted_tweet_classes[k])
+
+    return true, pred
+
+
+def calculate_results_at_each_depth(depthinfo, submission, test_truevals):
+
+    # Group true labels and predictions according to their depth
     # depthinfo id: depth
-
     # submission id: prediction
-
-    # test_truva; id: label
+    # test_truvals id: label
 
     depth_groups = {}
-
     depth_groups['0'] = []
     depth_groups['1'] = []
     depth_groups['2'] = []
@@ -259,8 +271,7 @@ if __name__ == "__main__":
     depth_groups['6+'] = []
 
 
-    # find all keys in that depth group
-
+    # Find all keys in that depth group
     for tweetid, tweetdepth in depthinfo.iteritems():
         if tweetdepth == 0:
             depth_groups['0'].append(tweetid)
@@ -277,10 +288,9 @@ if __name__ == "__main__":
         elif tweetdepth >5:
             depth_groups['6+'].append(tweetid)
 
-    ## make a list
+    # make a list
 
     depth_predictions = {}
-
     depth_predictions['0'] = []
     depth_predictions['1'] = []
     depth_predictions['2'] = []
@@ -290,7 +300,6 @@ if __name__ == "__main__":
     depth_predictions['6+'] = []
 
     depth_labels = {}
-
     depth_labels['0'] = []
     depth_labels['1'] = []
     depth_labels['2'] = []
@@ -305,9 +314,6 @@ if __name__ == "__main__":
         depth_predictions[depthgr] = [submission[x] for x in depth_groups[depthgr]]
         depth_labels[depthgr] = [test_truevals[x] for x in depth_groups[depthgr]]
 
-        test1 = [x for x in depth_groups[depthgr]]
-        test2 = [x for x in depth_groups[depthgr]]
-
         _, _, mactest_F, _ = precision_recall_fscore_support(depth_labels[depthgr],
                                                              depth_predictions[depthgr],
                                                              average='macro')
@@ -319,11 +325,114 @@ if __name__ == "__main__":
 
         depth_result[depthgr] = [mactest_F, mictest_F, test_F]
 
+    return depth_labels, depth_result
 
-    # Define some useful labels for table rows and columns
-    labels = ("Precision", "Recall", "F-score", "Support")
-    class_labels = ("Support", "Deny", "Query", "Comment")
-    class_labels_gap = ("",) + class_labels
+
+def print_table_three(true, pred, devtrue, devpred, best_trial_id, best_trial_loss):
+
+    print "\n\n--- Table 3 ---"
+
+    print "\nPart 1: Results on testing set"
+
+    print "\nAccuracy =", accuracy_score(true, pred)
+
+    print "\nMacro-average:"
+    macroavg_prfs = precision_recall_fscore_support(true, pred, average='macro')
+    for lab, val in zip(results_headers, macroavg_prfs):
+        if val is not None:
+            print "%-12s%-12.4f" % (lab, val)
+        else:
+            print "%-12s%-12s" % (lab, "--")
+
+    print "\nPer-class:"
+    perclass_prfs = precision_recall_fscore_support(true, pred)
+    print "%-12s%-12s%-12s%-12s%-12s" % class_labels_gap
+    for lab, vals in zip(results_headers, perclass_prfs):
+        if lab is "Support":
+            print "%-12s%-12i%-12i%-12i%-12i" % (lab, vals[0], vals[1], vals[2], vals[3])
+        else:
+            print "%-12s%-12.4f%-12.4f%-12.4f%-12.4f" % (lab, vals[0], vals[1], vals[2], vals[3])
+
+    print "\nPart 2: Results on development set"
+
+    if best_trial_id is not None:
+
+        print "\nBest trial =", best_trial_id, "with loss", best_trial_loss
+
+        #  Output is in the same format as the earlier part of Table 3
+        print "Accuracy =", accuracy_score(devtrue, devpred)
+
+        print "\nMacro-average:"
+        dev_macroavg_prfs = precision_recall_fscore_support(devtrue, devpred, average='macro')
+        for lab, val in zip(results_headers, dev_macroavg_prfs):
+            if val is not None:
+                print "%-12s%-12.4f" % (lab, val)
+            else:
+                print "%-12s%-12s" % (lab, "--")
+
+        print "\nPer-class:"
+        dev_perclass_prfs = precision_recall_fscore_support(devtrue, devpred)
+        print "%-12s%-12s%-12s%-12s%-12s" % class_labels_gap
+        for lab, vals in zip(results_headers, dev_perclass_prfs):
+            if lab is "Support":
+                print "%-12s%-12i%-12i%-12i%-12i" % (lab, vals[0], vals[1], vals[2], vals[3])
+            else:
+                print "%-12s%-12.4f%-12.4f%-12.4f%-12.4f" % (lab, vals[0], vals[1], vals[2], vals[3])
+
+    # trials.txt is generated by the parameter search, and so won't be generated by outer.py unless requested
+    # (substantial compute resources needed). So we may not be able to generate this part of the table.
+    else:
+        print "\nCould not find trials.txt; unable to generate results for development set in Table 3.\n"
+
+
+def print_extra_details(best_trial_id):
+
+    trials = pickle.load(open(os.path.join("output", "trials.txt"), "rb"))
+
+    # Print out the best combination of hyperparameters
+    print "\n--- New Table ---\n"
+    print "The best combination of hyperparameters, found in trial " + str(best_trial_id) + ", was:"
+    for param, param_value in trials.best_trial["result"]["Params"].iteritems():
+        print "\t", param, "=", param_value
+
+    # Let's examine the loss function at each iteration of the hyperparameter tuning process
+    print "\n--- New Figure ---"
+
+    # Extract the loss values from the full list of results, and calculate the running minimum value
+    loss = numpy.asarray([r["loss"] for r in trials.results])
+    running_min_loss = numpy.minimum.accumulate(loss)
+    lowest_loss = loss[best_trial_id]
+    all_best_ids = numpy.where(loss == lowest_loss)[0]
+
+    # Plot the loss and running loss values against the iteration number, and save to the output folder
+    plt.plot(range(0, len(loss)), loss, label="loss")
+    plt.plot(range(0, len(running_min_loss)), running_min_loss, label="running min(loss)")
+    plt.plot(best_trial_id, lowest_loss, "ro", label="min(loss)")
+    if len(all_best_ids) > 1:
+        plt.plot(all_best_ids, lowest_loss*numpy.ones(all_best_ids.shape), "rx", label="repeated min(loss)")
+    plt.legend()
+    plt.title("Hyperparameter optimisation")
+    plt.xlabel("Iteration")
+    plt.ylabel("Loss")
+    plt.savefig(os.path.join("output", "hyperparameter_loss_values.pdf"))
+
+    # Give details of other hyperparameter combinations that also achieved this loss
+    if len(all_best_ids) > 1:
+        print "\nWARNING: multiple hyperparameter combinations achieved the same lowest loss value as trial", best_trial_id
+        print "ID               ",
+        for id in all_best_ids:
+            print "%-17d" % id,
+        print ""
+        for param in trials.results[all_best_ids[0]]["Params"]:
+            print "%-17s" % param,
+            for id in all_best_ids:
+                print "%-17.4f" % trials.results[id]["Params"][param],
+            print ""
+
+    print "\nFigure showing hyperparameter optimisation progress can be found in the output folder.\n"
+
+
+def print_table_four(depth_labels, depth_result):
 
     print "\n\n--- Table 4 ---"
     print "\nNumber of tweets per depth and performance at each of the depths\n"
@@ -359,16 +468,10 @@ if __name__ == "__main__":
         print ""
 
 
+def print_table_five(true, pred):
+
     print "\n\n--- Table 5 ---"
     print "\nConfusion matrix\n"
-
-    true = []
-    pred = []
-
-    # Generate lists of true and predicted classes for all tweets in the test set
-    for k in test_truevals.keys():
-        true.append(test_truevals[k])
-        pred.append(submission[k])
 
     # Generate the confusion matrix and the list of labels (as above, in sorted class order as long as each class
     # appears once, which they all do).
@@ -382,92 +485,35 @@ if __name__ == "__main__":
         print "%-12s%-12i%-12i%-12i%-12i" % row
 
 
-    print "\n\n--- Table 3 ---"
+if __name__ == "__main__":
 
-    print "\nPart 1: Results on testing set"
+    # From the supplied files, load the tweet depth, predicted (test) labels, test labels and development labels.
+    tweet_depth, test_predicted_labels, test_labels, dev_labels = load_data()
 
-    print "\nAccuracy =", accuracy_score(true, pred)
+    # If it is present, load data from trials file and format in the same way as the submitted files
+    # (return None if the trials file is not available)
+    best_trial, best_loss, dev_predicted_labels = load_trials_data()
 
-    print "\nMacro-average:"
-    macroavg_prfs = precision_recall_fscore_support(true, pred, average='macro')
-    for lab, val in zip(labels, macroavg_prfs):
-        if val is not None:
-            print "%-12s%-12.4f" % (lab, val)
-        else:
-            print "%-12s%-12s" % (lab, "--")
+    # Analyse the results separately at each depth
+    level_for_each_depth, results_for_each_depth = \
+        calculate_results_at_each_depth(tweet_depth, test_predicted_labels, test_labels)
 
-    print "\nPer-class:"
-    perclass_prfs = precision_recall_fscore_support(true, pred)
-    print "%-12s%-12s%-12s%-12s%-12s" % class_labels_gap
-    for lab, vals in zip(labels, perclass_prfs):
-        if lab is "Support":
-            print "%-12s%-12i%-12i%-12i%-12i" % (lab, vals[0], vals[1], vals[2], vals[3])
-        else:
-            print "%-12s%-12.4f%-12.4f%-12.4f%-12.4f" % (lab, vals[0], vals[1], vals[2], vals[3])
+    # Get lists of the true and predicted classes for the test and, if possible, development sets
+    true_labels_test, predicted_labels_test = get_true_and_predicted_classes(test_labels, test_predicted_labels)
+    true_labels_dev, predicted_labels_dev = get_true_and_predicted_classes(dev_labels, dev_predicted_labels)
 
-    print "\nPart 2: Results on development set"
-    trials_file = os.path.join("output", "trials.txt")
+    # Define some useful labels for table rows and columns
+    results_headers = ("Precision", "Recall", "F-score", "Support")
+    class_labels = ("Support", "Deny", "Query", "Comment")
+    class_labels_gap = ("",) + class_labels
 
-    if os.path.exists(trials_file):
-        with open(trials_file, 'rb') as f:
-           trials = pickle.load(f)
+    # Print the tables
+    print_table_four(level_for_each_depth, results_for_each_depth)
+    print_table_five(true_labels_test, predicted_labels_test)
+    print_table_three(true_labels_test, predicted_labels_test,
+                      true_labels_dev, predicted_labels_dev, best_trial, best_loss)
 
-        # Extract results for dev data - we need to examine the best trial
-        best_trial_id = trials.best_trial["tid"]
-        print "\nBest trial =", best_trial_id, "with loss", trials.results[best_trial_id]["loss"]
-
-        dev_result_id = pickle.loads(trials.attachments["ATTACH::%d::ID" % best_trial_id])
-        dev_result_labels = pickle.loads(trials.attachments["ATTACH::%d::Labels" % best_trial_id])
-        dev_result_predictions = pickle.loads(trials.attachments["ATTACH::%d::Predictions" % best_trial_id])
-
-        # Change ID format from ints to strings
-        strpred = [convertlabeltostr(s) for s in dev_result_predictions]
-
-        # Transform to submission format and save
-        dev_results_dict = dict(zip(dev_result_id, strpred))
-        with open("result_dev.json", "w") as outfile:
-           json.dump(dev_results_dict, outfile)
-
-        devtrue = []
-        devpred = []
-
-        # Generate lists of true and predicted classes for all tweets in the development set
-        for k in dev.keys():
-            devtrue.append(dev[k])
-            devpred.append(dev_results_dict[k])
-
-        #  Output is in the same format as the earlier part of Table 3
-        print "Accuracy =", accuracy_score(devtrue, devpred)
-
-        print "\nMacro-average:"
-        dev_macroavg_prfs = precision_recall_fscore_support(devtrue, devpred, average='macro')
-        for lab, val in zip(labels, dev_macroavg_prfs):
-            if val is not None:
-                print "%-12s%-12.4f" % (lab, val)
-            else:
-                print "%-12s%-12s" % (lab, "--")
-
-        print "\nPer-class:"
-        dev_perclass_prfs = precision_recall_fscore_support(devtrue, devpred)
-        print "%-12s%-12s%-12s%-12s%-12s" % class_labels_gap
-        for lab, vals in zip(labels, dev_perclass_prfs):
-            if lab is "Support":
-                print "%-12s%-12i%-12i%-12i%-12i" % (lab, vals[0], vals[1], vals[2], vals[3])
-            else:
-                print "%-12s%-12.4f%-12.4f%-12.4f%-12.4f" % (lab, vals[0], vals[1], vals[2], vals[3])
-
-        # New - print out the best combination of hyperparameters
-        print "\n--- New Table ---\n"
-        print "The best combination of hyperparameters, found in trial " + str(best_trial_id) + ", was:"
-        for param, param_value in trials.best_trial["result"]["Params"].iteritems():
-            print "\t", param, "=", param_value
-
-        # New - let's examine the loss function at each iteration of the hyperparameter tuning process
-        print "\n--- New Figure ---"
-        examine_hyperparameter_optimisation(trials.results, best_trial_id)
-        print "\nFigure showing hyperparameter optimisation progress can be found in the output folder.\n"
-
-    # trials.txt is generated by the parameter search, and so won't be generated by outer.py unless requested
-    # (substantial compute resources needed). So we may not be able to generate this part of the table.
-    else:
-        print "\nCould not find trials.txt; unable to generate results for development set in Table 3.\n"
+    # If the trials file is available, output more details of the best hyperparameter combinations and prepare a figure
+    # showing the loss during the hyperparameter choice process
+    if best_trial is not None:
+        print_extra_details(best_trial)
